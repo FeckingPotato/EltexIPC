d4f710348cf2beefd2962f424b2a2f78ec4fc46d
FeckingPotato <cornilow.serega@gmail.com>
2023-07-24 17:13:53 +0700

    4.3 done

---
+++ 4/4.3/README.md
@@ -0,0 +1,4 @@
+# CMake projects:
+
+- 4.3 - writes random numbers to output.txt then its children output 'output.txt' to stdout, syntax:
+  - ./4.3 AMOUNT_OF_NUMBERS
---
+++ 4/4.3/main.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <wait.h>
+#include "semaphore_wrapper.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Wrong amount of arguments\n");
+        return 1;
+    }
+    long amount = strtol(argv[1], NULL, 10);
+    srand(time(NULL));
+    FILE* output;
+    output = fopen("output.txt", "w");
+    fclose(output);
+    int semid = semGet(CUR_PROJ);
+    switch (fork()) {
+        case -1:
+            printf("Unable to fork\n");
+            semDestroy(semid);
+            return 1;
+        case 0: {
+            if (fork() == -1 || fork() == -1 || fork() == -1 || fork() == -1) {
+                printf("Unable to fork\n");
+                return 1;
+            }
+            semLock(semid);
+            output = fopen("output.txt", "r");
+            fseek(output, 0, SEEK_END);
+            long size = ftell(output);
+            fseek(output, 0, SEEK_SET);
+            char buffer[size+1];
+            fread(buffer, 1, size, output);
+            fclose(output);
+            semUnlock(semid);
+            buffer[size] = '\0';
+            printf("%s\n", buffer);
+            return 0;
+        }
+        default:
+            semLock(semid);
+            output = fopen("output.txt", "a");
+            for (long i = 0; i < amount; i++) {
+                int random = rand() % 2000 - 1000;
+                fprintf(output, "%d ", random);
+                fflush(output);
+            }
+            fclose(output);
+            semUnlock(semid);
+            wait(NULL);
+            semDestroy(semid);
+            return 0;
+    }
+}
